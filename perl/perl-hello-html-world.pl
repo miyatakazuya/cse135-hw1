7243223a3e00c1f5335c9e5091f5ebbbaae75767
miyatakazuya <kazuyamiyata18@gmail.com>
2026-02-03 02:13:25 +0000

    server-side sync

--- perl/perl-hello-html-world.pl
+++ perl/perl-hello-html-world.pl
@@ -11,12 +11,11 @@ print "</head>";
 print "<body>";
 
 print "<h1 align=center>Hello HTML World</h1><hr/>";
-print "<p>Hello World</p>";
+print "<h2>Perl Hello World - By Kazuya Miyata CSE135</h2>";
 print "<p>This page was generated with the Perl programming langauge</p>";
 
 $date = localtime();
 print "<p>This program was generated at: $date</p>";
-
 # IP Address is an environment variable when using CGI
 $address = $ENV{REMOTE_ADDR};
 print "<p>Your current IP Address is: $address</p>";
